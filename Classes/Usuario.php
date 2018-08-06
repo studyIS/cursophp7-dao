dd8870dd098979a793869796012402e3fd14b54d
Rayara Marinho <rayaramarinho@hotmail.com>
2018-08-05 22:14:27 -0400

    Métodos de Lisa

--- Classes/Usuario.php
+++ Classes/Usuario.php
@@ -71,8 +71,40 @@ class Usuario{
 
 	}// fim toString
 
+	public static function getList(){
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
 
-}
+	}//fim getList
+
+	public static function search($login){
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(':SEARCH'=>"%".$login."%" ));
+
+	}//fim search
+
+	function login($login, $password){
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(":LOGIN"=>$login, ":PASSWORD"=>$password
+	));
+
+
+		if (count($results)>0) {
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		}else{
+				throw new Exception("Login e/ou Senha Inválidos");
+		}// fim else
+
+			
+	}// fim login
+
+}// fim classe Usuario
 
 
 
--- index.php
+++ index.php
@@ -2,19 +2,41 @@
 
 require_once("config.php");
 
+//CARREGA UM USUARIO PELO LOGIN E SENHA
+
+$usuario = new Usuario();
+$usuario->login("Rayara","7477");
+
+echo $usuario;
+
+
+/*	CARREGA UMA LISTA DE USUARIOS BUSCANDO PELO LOGIN
+$search = Usuario::search("R");
+
+echo json_encode($search);
+*/
+
+/*
+$lista = Usuario::getList();
+echo json_encode($lista);
+
+
+/*
+	CARREGA UM USUARIO
 $root = new Usuario();
 
 $root->loadById(2);
 
 echo $root;
+*/
 
 /*
+	CARREGA TODOS USUARIOS
 $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
-
 
 */
  ?>
